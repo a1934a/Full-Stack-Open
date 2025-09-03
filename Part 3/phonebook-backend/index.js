da356201f91f146c008ecbd296bb68e6834c362e
Richard Drake <richard_ma2005@163.com>
2025-09-03 16:20:14 +0800

    distribution

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('content', function (req, res) { 
     return JSON.stringify(req.body)
