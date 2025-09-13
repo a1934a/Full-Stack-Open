8e2725c8ef06a8ccbdce912a34699b51807a5f17
Richard Drake <richard_ma2005@163.com>
2025-09-13 14:24:36 +0800

    uibuild

--- Part 3/phonebook-backend/models/person.js
+++ Part 3/phonebook-backend/models/person.js
@@ -9,15 +9,25 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
     name: {
-        type : String,
-        minLength : 3,
+        type: String,
+        minLength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: (v) => {
+                return /\d{2,3}-\d+/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true,
+    },
 })
 
-personSchema.set('toJSON',{
-    transform:(document,returnedObject)=>{
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
         returnedObject.id = document._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
